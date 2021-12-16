810205dedae6bfa933af28a8e2dd873de886d78d
Austin1a2b <cavalier283@gmail.com>
2021-12-16 14:28:58 +0800

    完成首頁到 顯示短網址網頁 的路由 , 且完成  input 網址 相同 與不同 的判斷

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
-const { engine } = require('express/lib/application')
 
 const app = express()
+app.use(express.urlencoded({ extended: true }))
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
@@ -12,9 +12,36 @@ const db = mongoose.connection
 
 const port = 3000
 
+const Record = require('./models/record')
+
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+app.post('/show', (req, res) => {
+  const inputURL = req.body.URL
+  let shortURL = ''
+  //1. 先檢查 資料庫 有無 相同 URL
+  Record.find()
+    .lean()
+    .then(dataArry => {
+      const result = dataArry.find(record => {
+        return record.originURL === inputURL
+      })
+      // 後續用來判斷  是否有重複 , 
+      const length = result || 'noRepeat'
+      if (length !== 'noRepeat') {
+        shortURL = result.shortURL
+      } else {
+        //2. 無相同URL 尚未製作 亂碼器
+        shortURL = 'random URL'
+        Record.create({ originURL: inputURL, shortURL: 'http://localhost/3000/test3' })
+      }
+      res.render('show', { shortURL: shortURL })
+    })
+})
+
+
 
 db.on('error', () => {
   console.log('mongodb error!')

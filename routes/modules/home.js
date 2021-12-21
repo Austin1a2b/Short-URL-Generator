8ad7fc6faf349740d0d3f3fba865feda406f8088
Austin1a2b <cavalier283@gmail.com>
2021-12-22 00:24:34 +0800

    將 filter 篩選 方式  改為 使用資料庫find() 功能

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -6,24 +6,19 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
-
 //輸入短網址 => 到其他網站
 router.get('/:random', (req, res) => {
   const random = req.params.random
   const inputshortURL = `http://localhost:3000/${random}`
-  Record.find()
+  Record.find({ shortURL: inputshortURL })
     .lean()
-    .then(dataArry => {
-      const result = dataArry.find(record => {
-        return record.shortURL === inputshortURL
-      })
-      const judgment = result || 'noData'
-      if (judgment !== 'noData') {
-        res.redirect(result.originURL)
+    .then(data => {
+      console.log(data)
+      if (data.length !== 0) {
+        res.redirect(data[0].originURL)
       } else {
-        res.render('show', { Nodata: judgment })
+        res.render('show', { Nodata: 'Nodata' })
       }
     })
 })
-
 module.exports = router
--- routes/modules/show.js
+++ routes/modules/show.js
@@ -9,19 +9,13 @@ router.post('/', (req, res) => {
   const inputURL = req.body.URL
   let shortURL = ''
   //1. 先檢查 資料庫 有無 相同 URL  
-  Record.find()
+  Record.find({ originURL: inputURL })
     .lean()
-    .then(dataArry => {
-      const result = dataArry.find(record => {
-        return record.originURL === inputURL
-      })
-      // 後續用來判斷  是否有重複 , 
-      const judgment = result || 'noRepeat'
-      //---輸入相同網址時，產生一樣的縮址。
-      if (judgment !== 'noRepeat') {
-        shortURL = result.shortURL
+    .then(data => {
+      //輸入相同網址時，產生一樣的縮址。
+      if (data.length !== 0) {
+        shortURL = data[0].shortURL
       } else {
-        //2. 無相同URL 則給 網址+5碼英數亂碼        
         shortURL = `http://localhost:3000/${randomString(5)}`
         Record.create({ originURL: inputURL, shortURL: shortURL })
       }
